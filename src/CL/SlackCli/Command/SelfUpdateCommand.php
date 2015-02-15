ee395bb5a9d6d9ae3c2b05bc04ff1f0604dcf95b
Cas Leentfaar <git@casleentfaar.com>
2015-02-16 00:59:03 +0100

    adjusted update conditions

--- src/CL/SlackCli/Command/SelfUpdateCommand.php
+++ src/CL/SlackCli/Command/SelfUpdateCommand.php
@@ -4,6 +4,8 @@ namespace CL\SlackCli\Command;
 
 use Herrera\Phar\Update\Manager;
 use Herrera\Phar\Update\Manifest;
+use Herrera\Version\Parser;
+use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -17,7 +19,7 @@ class SelfUpdateCommand extends AbstractCommand
     protected function configure()
     {
         parent::configure();
-        
+
         $this->setName('self:update');
         $this->setDescription('Updates slack.phar to the latest version');
     }
@@ -27,18 +29,39 @@ class SelfUpdateCommand extends AbstractCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $currentVersion = $this->getApplication()->getVersion();
-        $manager        = new Manager(Manifest::loadFile(self::MANIFEST_FILE));
-        
-        if ($manager->update($currentVersion)) {
+        $currentVersion  = $this->getApplication()->getVersion();
+        $manager         = new Manager(Manifest::loadFile(self::MANIFEST_FILE));
+        $lockMajor       = false;
+        $allowPreRelease = false;
+
+        if (substr($currentVersion, 0, 1) == 0) {
+            $allowPreRelease = true;
+        }
+
+        if ($manager->update($currentVersion, $lockMajor, $allowPreRelease)) {
+            $newVersionObject = $manager->getManifest()->findRecent(
+                Parser::toVersion($currentVersion),
+                $lockMajor,
+                $allowPreRelease
+            )->getVersion();
+
+            $newVersion = sprintf(
+                '%s.%s',
+                $newVersionObject->getMajor(),
+                implode('.', array_filter([
+                    $newVersionObject->getMinor(),
+                    $newVersionObject->getPatch()
+                ]))
+            );
+
             $output->writeln(sprintf(
-                '<info>Updated Slack CLI from <fg=yellow>%s</fg=yellow> to <fg=yellow>%s</fg=yellow></info>', 
-                $currentVersion, 
-                $this->getApplication()->getVersion()
+                '<info>Updated Slack CLI from <fg=yellow>%s</fg=yellow> to <fg=yellow>%s</fg=yellow></info>',
+                $currentVersion,
+                $newVersion
             ));
         } else {
             $output->writeln(sprintf(
-                '<comment>You are already using the latest version (%s)</comment>', 
+                '<comment>You are already using the latest version (%s)</comment>',
                 $currentVersion
             ));
         }
